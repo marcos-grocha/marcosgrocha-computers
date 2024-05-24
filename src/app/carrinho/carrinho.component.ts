7b29eb93528652a866b54232930e92dcd6c2e0de
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2024-05-24 01:36:41 -0300

    adicionado função remover item do carrinho e refatoração de detalhes no carrinho

--- src/app/carrinho/carrinho.component.ts
+++ src/app/carrinho/carrinho.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CarrinhoService } from '../carrinho.service';
+import { IProdutoCarrinho } from '../produtos';
 
 @Component({
   selector: 'app-carrinho',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carrinho.component.css']
 })
 export class CarrinhoComponent implements OnInit {
+  itensCarrinho: IProdutoCarrinho[] = [];
+  total = 0;
 
-  constructor() { }
+  constructor(
+    public carrinhoService: CarrinhoService
+  ) { }
 
   ngOnInit(): void {
+    this.itensCarrinho = this.carrinhoService.obtemCarrinho();
+    this.calcularTotal();
   }
 
+  calcularTotal() {
+    this.total = this.itensCarrinho.reduce((prev, curr) => prev + (curr.preco * curr.quantidade), 0);
+  }
+
+  removeProdutoCarrinho(produtoId: number) {
+    this.itensCarrinho = this.itensCarrinho.filter(item => item.id !== produtoId);
+    this.carrinhoService.removerProdutoCarrinho(produtoId);
+    this.calcularTotal();
+  }
 }
